3ef04e4af507a449c87d00d42045fbe331aa6c2b
Айдан Татышева <aidan@Ajdans-MacBook-Pro.local>
2020-01-17 10:29:12 +0600

    hero section added

--- dist/style.6b54f5b7.js
+++ dist/style.6b54f5b7.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/hero.png":[["hero.c0a374e7.png","img/hero.png"],"img/hero.png"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63121" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61355" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
